4512779b183214a360741b6db22547a3b577c948
mayor <homer.c.simpson50@gmail.com>
2026-04-05 00:25:51 -0500

    fix: resize window to 80% of screen via iTerm2 bounds

--- scripts/gastown-workspace.sh
+++ scripts/gastown-workspace.sh
@@ -72,34 +72,9 @@ tell application "iTerm2"
         end tell
     end tell
 
-    -- Resize: make the window ~80% of screen
-    tell current window
-        set screenBounds to bounds of current window
-        tell application "Finder"
-            set screenSize to bounds of window of desktop
-        end tell
-    end tell
-end tell
+    -- Resize window to ~80% of screen (1440x900 → 1152x720, centered)
+    set bounds of current window to {144, 90, 1296, 810}
 
--- Resize window to 80% of screen using System Events
-tell application "System Events"
-    tell process "iTerm2"
-        set frontWindow to front window
-        set {screenW, screenH} to {1440, 900}
-        try
-            tell application "Finder"
-                set screenSize to bounds of window of desktop
-                set screenW to item 3 of screenSize
-                set screenH to item 4 of screenSize
-            end tell
-        end try
-        set targetW to (screenW * 0.8) as integer
-        set targetH to (screenH * 0.8) as integer
-        set targetX to ((screenW - targetW) / 2) as integer
-        set targetY to ((screenH - targetH) / 2) as integer
-        set position of frontWindow to {targetX, targetY}
-        set size of frontWindow to {targetW, targetH}
-    end tell
 end tell
 APPLESCRIPT
 
